9a002cf567dde2d23ca5cd43a1014c58df4e2ed2
connorkelley <kelleyconnor@outlook.com>
2015-02-25 10:43:05 -0800

    Signed-off-by: connorkelley <kelleyconnor@outlook.com>

---
+++ Lab/HelloWorldGradeF/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on February 25, 2015, 10:39 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    <<cout "Hello World!"<< endl;
+    return 0;
+}
+
