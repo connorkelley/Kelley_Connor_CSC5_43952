bc7104be3618409219be13b3ee68e85883fddfd3
connorkelley <kelleyconnor@outlook.com>
2015-03-04 08:58:00 -0800

    Connor Kelley

--- Homework/Assignment 1/Gaddis_8thEd_Chap2_Prob18/main.cpp
+++ Homework/Assignment 1/Gaddis_8thEd_Chap2_Prob18/main.cpp
@@ -19,7 +19,7 @@ unsigned char CNVPCT = 100;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    unsigned short nCust, nEDrnks, nCDrnks;
+    unsigned short nCust, nEDrnks, nCDrnks; //Will not be a negative value ever - should use unsigned
     unsigned char pEDrnks, pCDrnks; 
     //Input / Assigned Values
     nCust = 16500;  //Number of Customers
@@ -27,8 +27,8 @@ int main(int argc, char** argv) {
     pCDrnks = 58;   //Percentage out of Energy Drinkers that prefer citrus drinks
     
     //Process / Calculate
-    nEDrnks = nCust/static_cast<float>(CNVPCT)*pEDrnks;
-    nCDrnks = nEDrnks/static_cast<float>(CNVPCT)*pCDrnks;
+    nEDrnks = nCust/static_cast<float>(CNVPCT)*pEDrnks+0.5;     //Round Up
+    nCDrnks = nEDrnks/static_cast<float>(CNVPCT)*pCDrnks+0.5;   //Round Up
     //Output Everything
     cout<<"Energy Drinks Customer Survey Problem"<<endl;
     cout<<"Known/Given Values: "<<endl;
