661c9501b88d0c3f823fac0e7e3802f3b48cbb9f
connorkelley <kelleyconnor@outlook.com>
2015-03-18 10:02:09 -0700

    Connor Kelley <kelleyconnor@outlook.com>

---
+++ Lab/TruthTable/main.cpp
@@ -0,0 +1,99 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on March 18, 2015, 9:37 AM
+ * Purpose: Determine if Table created in class 
+ *          agrees with logic of the computer
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declare Variables 
+    bool x, y;
+    
+    //Heading
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X"
+        <<" !(X&&Y) !X||!Y !(X||Y) !X&&!Y"<<endl;
+    
+    //Output the first row of the table
+    x=true;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"   ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F');
+    cout<<endl;
+    
+    //Output the second row of the table
+    x=true;y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"   ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F');
+    cout<<endl;
+    
+    //Output the third row of the table
+    x=false;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"   ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F');
+    cout<<endl;
+    
+    //Fourth row of the table
+    x=false;y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"   ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F');
+    cout<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
