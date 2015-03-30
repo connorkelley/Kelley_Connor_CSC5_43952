7df6855d889540d8869b40003bdb17a427e08619
connorkelley <kelleyconnor@outlook.com>
2015-03-30 10:47:38 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Homework/Assignment 4/Gaddis_8thEd_Chap5_Prob1/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on March 30, 2015, 10:39 AM
+ * Purpose: Homework Assignment 4 - 1st Problem - 1. Sum of Numbers
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Loop for all possible digits
+    
+    //Exit Stage Right!
+    return 0;
+}
+
