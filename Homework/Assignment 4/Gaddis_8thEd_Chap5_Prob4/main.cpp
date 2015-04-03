3ce15578e2f2e0838773043596914911f69ff00e
connorkelley <kelleyconnor@outlook.com>
2015-04-02 17:08:50 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Homework/Assignment 4/Gaddis_8thEd_Chap5_Prob4/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on April 2, 2015, 4:55 PM
+ * Purpose: Homework Assignment 4 - 3rd Problem - 4. Calories Burned
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float cal = 3.6, sum;    //Calories burned per minute on a treadmill
+    
+    //Loop to desired value
+    for(int counter =5; counter <=30; counter+=5){ //'x' = minutes on the treadmill
+        sum = cal*counter;
+        cout<<"After "<<counter<<" minutes, you will burn "<<sum<<" calories on the treadmill."<<endl;
+        
+    }
+    
+    //Exit Stage Right!
+    return 0;
+}
+
