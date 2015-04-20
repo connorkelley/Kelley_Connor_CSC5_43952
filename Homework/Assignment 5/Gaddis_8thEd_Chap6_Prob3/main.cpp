c13c4686a95b9f27cc7e65390c03aba1702e9f19
connorkelley <kelleyconnor@outlook.com>
2015-04-20 10:55:54 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Homework/Assignment 5/Gaddis_8thEd_Chap6_Prob3/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on April 20, 2015, 10:50 AM
+ * Purpose: Homework Assignment 5: 3. Winning Division
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+double getSales();
+void findHighest();
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Call Functions
+    getSales();
+    findHighest();
+    
+    //Exit Stage Right!
+    return 0;
+}
+
+double getSales(){  
+    //Passed name of division
+    //Asks the user for a quarterly sales figure
+    //Validates the input (Nothing less than $0.00)
+}
+
+void findHighest(){
+    //Is passed the four sales totals
+    //Determines which is the largest
+    //Prints the name of the high grossing division & Sales Figure
+}
