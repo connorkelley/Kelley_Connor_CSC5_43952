5377e28c5fe9bb405c6e797a3af0e5f422479d68
connorkelley <kelleyconnor@outlook.com>
2015-05-06 10:53:47 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Projects/Pokemon_Project1/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on May 6, 2015, 10:04 AM
+ * Purpose: Create a functional game
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+void gameStr();
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    
+    //Start Game
+    gameStr();
+    //Exit Stage Right!
+    return 0;
+}
+
+void gameStr(){
+    cout<<"******************\n"
+        <<"Welcome to Pokemon\n"
+        <<"  Push A to start \n"
+        <<"******************\n";
+}
