d2c70179bc25702670f5289c7ce37d322453e0d4
connorkelley <kelleyconnor@outlook.com>
2015-05-11 16:17:24 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob18/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Kelley
+ * Created on May 11, 2015, 3:44 PM
+ * Purpose: Homework Assignment 6: 18. Tic-Tac-Toe Game
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    const int row = 3;
+    const int col = 3;
+    char board[row][col] = {};
+    int user1, user2;
+    
+    //Initialize the array to display *'s
+    for (int x = 0; x < row; x++){
+        for(int y = 0; y < col; y++){
+            board[x][y] = '*';
+        }
+    }
+    //Display the board
+    for (int x = 0; x < row; x++){
+        for(int y = 0; y < col; y++){
+            cout<<board[x][y]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    //Ask for input from user 1
+    cout<<"User 1, enter a row and column to put an X"<<endl;
+    cin>>board[user1][user1];
+    
+    
+    //Exit Stage Right!
+    return 0;
+}
+
--- Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob5/main.cpp
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob5/main.cpp
@@ -22,33 +22,39 @@ int main(int argc, char** argv) {
     
     const int mnkyNum = 3;  //3 Rows
     const int weekDay = 5;  //5 Columns
-    int mnkyAry[mnkyNum][weekDay];  //3x5 Array
-    int total, mnkyAvg;
+    int mnkyAry[mnkyNum][weekDay] = {};  //3x5 Array
+    int total, mnkyAvg, small, big;
     
     //Input data for each monkey
     cout<<"Enter the amount of food eaten per day by each of the following monkeys:"<<endl;
     for(int mnky =0; mnky < mnkyNum; mnky++){
         for(int day =0; day < weekDay; day++){
-            cout<<"Monkey "<<mnky+1<<", Weekday "<<day+1<<": ";
-            cin>> mnkyAry[mnkyNum][weekDay];
+            cout<<"Monkey "<<(mnky+1)<<", Weekday "<<(day+1)<<": ";
+            cin>>(mnkyAry[mnky][day]);
         }
     }
-    
     //Create a report that includes 
-        //Average amount of food per day by the whole family of monkeys
+    //Average amount of food per day by the whole family of monkeys
     for (int row = 0; row < mnkyNum; row++){
         total = 0;
         //Sum the Row
-        for (int col = 0; col < weekDay; col++){
-            total += mnkyAry[mnkyNum][weekDay];
-        }
+        for (int col = 0; col < weekDay; col++) total += mnkyAry[row][col];
         mnkyAvg = total / weekDay;
-        cout<<"Average amount of food eaten for monkey "<<row+1<<mnkyAvg<<endl;
+        cout<<"Average amount of food eaten for monkey "<<(row+1)<<": "<<mnkyAvg<<endl;
     }
-        //Least amount of food eaten during the week by any one monkey
-        //Greatest amount of food eaten during the week by any one monkey
-    
-    //Don't accept negative numbers for food eaten
+    //Calculate the most and least amount of food eaten by any one monkey
+    small = mnkyAry[0][0];
+    big = mnkyAry[0][0];
+    for (int row = 0; row < mnkyNum; row++){
+        for (int col = 0; col < weekDay; col++){
+            if(mnkyAry[row][col] < small) small = mnkyAry[row][col];
+            if(mnkyAry[row][col] > big)   big   = mnkyAry[row][col];
+        }
+    }
+    //Least amount of food eaten during the week by any one monkey
+    cout<<"The least amount of food eaten by any monkey was "<<small<<"."<<endl;
+    //Greatest amount of food eaten during the week by any one monkey
+    cout<<"The most  amount of food eaten by any monkey was "<<big<<"."<<endl;
     
     //Exit Stage Right!
     return 0;
