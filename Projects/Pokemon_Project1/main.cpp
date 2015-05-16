f2674d27a40ad03457d74513b45147c4628eacc7
connorkelley <kelleyconnor@outlook.com>
2015-05-15 22:44:31 -0700

    connor kelley <kelleyconnor@outlook.com>

--- Projects/Pokemon_Project1/main.cpp
+++ Projects/Pokemon_Project1/main.cpp
@@ -30,9 +30,9 @@ int battle(int, int, int);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Player Variables
-    int     pmExp   = 0,  //Pokemon Experience
-            pmHp    = 100,//Pokemon Hit points
-            pmLvl   = 1;  //Pokemon Level
+    int     pmExp   = 0;  //Pokemon Experience
+    int     pmLvl   = 1, 
+            pmHp    = 100;
     string  name;   //Player Name
     //Other Variables
     int userSel;        
@@ -41,6 +41,24 @@ int main(int argc, char** argv) {
     
     //Start Game
     gameStr();
+    
+    //Calculate HP based on level...
+    //Then I would need to pass the array to 3-4 functions to save, load, and use it in battle...
+        //Could get overly complicated
+    //const int SIZE = 5;
+    //int pmInfo[SIZE][2] = {};
+    //Use this array for something... Not sure yet how practical this is
+    //for(int x = 0; x < SIZE; x++){
+    //    for(int y = 0; y < 2; y++){
+    //        pmInfo[x][y] = 100*(x+1);
+    //    }
+    //}
+    //for(int x = 0; x < SIZE; x++){
+    //    for(int y = 0; y < 2; y++){
+    //        cout<<pmInfo[x][y]<<endl;
+    //    }
+    //}
+    
     //Game Menu to load or start new game    
     do{   
         cout<<"********     MENU     *********"<<endl;
@@ -60,28 +78,28 @@ int main(int argc, char** argv) {
         
     //Start the in-game menu...
     while(inGame){
-    char choice;
-    cout<<"What would you like to do?"<<endl;
-    cout<<"[1.] Battle a Pokemon"<<endl;
-    cout<<"[2.] Save the game"<<endl;
-    cout<<"[3.] Exit the game"<<endl;
-    cin>>choice;
-    switch (choice){
-        case '1':{
-            battle(pmExp, pmHp, pmLvl); 
-            break;
+        char choice;
+        cout<<"What would you like to do?"<<endl;
+        cout<<"[1.] Battle a Pokemon"<<endl;
+        cout<<"[2.] Save the game"<<endl;
+        cout<<"[3.] Exit the game"<<endl;
+        cin>>choice;
+        switch (choice){
+            case '1':{
+                battle(pmExp, pmHp, pmLvl); 
+                break;
+            }
+            case '2':{
+                savGame(pmExp, pmHp, pmLvl); 
+                cout<<"Your game has saved!"<<endl;
+                break;
+            }
+            case '3':{
+                exit(0);
+                inGame = false;
+                break;
+            }
         }
-        case '2':{
-            savGame(pmExp, pmHp, pmLvl); 
-            cout<<"Your game has saved!"<<endl;
-            break;
-        }
-        case '3':{
-            exit(0);
-            inGame = false;
-            break;
-        }
-    }
     }
     //Exit Stage Right!
     return 0;
@@ -163,30 +181,21 @@ int battle(int pmExp, int pmHp, int pmLvl){
             fStrng, //Foe's Strength Value
             fLvl,   //Foe's Level
             pmStrng;//Pokemon Strength   
-    const int SIZE = 5;
-    int hpAry[SIZE] = {};
-    int randNum = rand()%100+75; 
     
     //Calculate the enemy's level
     if((pmLvl >= 1)&&(pmLvl <= 5)){
         fLvl = rand()%5+1;
+        fHp  = rand()%100+75;
     } else if ((pmLvl >=6)&&(pmLvl <=10)){
         fLvl = rand()%10+5;
-    }
-    
-    //random array to calculate enemy health
-    for(int count = 1; count <= SIZE; count ++){
-        hpAry[count] += (randNum*count);
-    }
-    for(int count = 0; count < SIZE; count++){
-        cout<<hpAry[count]<<" ";
+        fHp  = rand()%200+150;
     }
     
     //Output Battle Sequence...
     cout<<endl<<"You've encountered a foe!"<<endl;
     //Output Enemy Stats!
     cout<<"Enemy level   : "<<fLvl<<endl;
-    cout<<"Enemy HP      : "<<hpAry[rand()%4]<<endl;
+    cout<<"Enemy HP      : "<<fHp<<endl;
     cout<<"Enemy Strength: "<<fStrng<<endl;
     do{
         char select;
@@ -203,16 +212,18 @@ int battle(int pmExp, int pmHp, int pmLvl){
                 cout<<"Success! You attack the enemy."<<endl;
                 cout<<"You hit the enemy for "<<pmStrng<<" damage!"<<endl;
                 cout<<"The enemy HP is now at: "<<fHp<<endl<<endl;
-                cout<<"The enemy attacks you!"<<endl<<"You get hit for "<<fStrng<<" damage."<<endl;
-                pmHp -= fStrng;
-                cout<<"Your HP is now at: "<<pmHp<<endl;
+                if(fHp > 0){
+                    cout<<"The enemy attacks you!"<<endl<<"You get hit for "<<fStrng<<" damage."<<endl;
+                    pmHp -= fStrng;
+                    cout<<"Your HP is now at: "<<pmHp<<endl;
+                }    
                 break;
             }
             case '2':{
                 int randNum = rand()%10+1;
                 if(randNum > 5){
                     cout<<"You ran away safely..."<<endl;
-                    fHp = -1;
+                    fHp = -99;
                 } else {
                     cout<<"Oh no! You failed to run away!!"<<endl;
                     cout<<"The enemy attacks you! You get hit for "<<fStrng<<" damage."<<endl;
@@ -222,22 +233,28 @@ int battle(int pmExp, int pmHp, int pmLvl){
                 break;
             }
         }
-    } while ((fHp > 0)&&(pmHp > 0));
-    //Determine win or lose
-    if(pmHp > 0){
-        cout<<"You lost the battle. You lose a level!"<<endl;
-        pmLvl--;
-        cout<<"Your current level is now at "<<pmLvl<<endl;
-    } else if (fHp > 0){
+    } while ((fHp > 0)&&(pmHp > 0)&&(fHp != -99));
+    
+    //Determine win or lose or run
+    if((pmHp < 0)&&(fHp > 0)&&(fHp != -99)){
+        cout<<"You lost the battle."<<endl;
+        if (pmLvl > 1){
+            cout<<"You lost a level!"<<endl;
+            pmLvl--;
+            cout<<"Your current level is now at "<<pmLvl<<endl;
+        } else cout<<"Your level is too low to lose a level!"<<endl;
+    } else if ((fHp < 0)&&(pmHp > 0)&&(fHp != -99)){
         cout<<"CONGRATS! YOU WON THE BATTLE!"<<endl;
         pmExp += rand()%100+75;
-        cout<<"You gained "<<pmExp<<" for winning!"<<endl;
+        cout<<"You gained "<<pmExp<<" experience for winning!"<<endl;
+    } else if (fHp = -99){
+        cout<<"Because you ran away from combat,"<<endl;
+        cout<<"You neither gained nor lost experience."<<endl;
     }
     
     //Determine Level w/ experience
     if(pmExp >= 100){
         pmLvl++;
-        pmHp += 100;
     }
         
     //Exit Stage Right!
