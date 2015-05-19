be716c192ec267cf89f02b69da49fe08cdc82b67
connorkelley <kelleyconnor@outlook.com>
2015-05-19 12:18:40 -0700

    connor kelley <kelleyconnor@outlook.com>

---
+++ Projects/Pokemon_Project2_V2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor
+ *
+ * Created on May 19, 2015, 11:13 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
