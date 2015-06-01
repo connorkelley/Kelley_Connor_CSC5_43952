79fd893e6f4c4b9a2a4685cb8a5782dba87c2b82
connorkelley <kelleyconnor@outlook.com>
2015-06-01 10:52:49 -0700

    connor kelley <kelleyconnor@outlook.com>

--- Lab/MultiplicationTableDynamicArray/main.cpp
+++ Lab/MultiplicationTableDynamicArray/main.cpp
@@ -1,19 +1,77 @@
 /* 
  * File:   main.cpp
- * Author: Connor
- *
+ * Author: Connor Kelley
  * Created on June 1, 2015, 8:35 AM
+ * Purpose: 2D Dynamic Array
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+
+//Function Prototypes
+int **filAray(int,int);
+void prntAry(int **,int,int);
+void destroy(int **,int);
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Create the array
+    int rows=10,cols=11;
+    int **array=filAray(rows,cols);
+    //Print the array
+    prntAry(array,rows,cols);
+    //Deallocate the array
+    destroy(array,rows);
+    //Exit stage right
     return 0;
 }
 
+void destroy(int **a,int r){
+    //Loop and destroy the columns
+    for(int i=0;i<r;i++){
+        delete [] a[i];
+    }
+    //Destroy the rows
+    delete []a;
+}
+
+void prntAry(int **a,int r,int c){
+    //Print the headings
+    cout<<"THIS IS YOUR MULTIPLICATION TABLE!!!"<<endl;
+    cout<<endl<<setw(8)<<0;
+    for(int i=1;i<c;i++){
+        cout<<setw(4)<<i;
+    }
+    cout<<endl;
+    //Print the array
+    for(int i=0;i<r;i++){
+        cout<<setw(4)<<i;
+        for(int j=0;j<c;j++){
+            cout<<setw(4)<<a[i][j];
+        }
+        cout<<endl;
+    }
+}
+
+int **filAray(int row,int col){
+    //Create the number of rows
+    int **array=new int*[row];
+    //Loop and create the columns
+    for(int i=0;i<row;i++){
+        array[i]=new int[col];
+    }
+    //Fill the array
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            array[i][j]=i*j;
+        }
+    }
+    //Return the array
+    return array;
+}
--- Projects/PlayerArray/main.cpp
+++ Projects/PlayerArray/main.cpp
@@ -14,19 +14,14 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void savData(int[][], const int);
+void savGame(int[][]);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 10;
-    int plyrDta[SIZE][3];
+    int plyrDta[][];
+    
     
     //Exit Stage Right!
     return 0;
-}
-
-void savData(int plyrDta[][3], const int SIZE){
-    //Fill the array
-    
 }
